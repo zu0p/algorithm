eceeb1be44d90b3727304abc89ee4f5b611b2ee9
박주영 <zu0p@kakao.com>
2021-01-18 22:45:11 +0900

    210118 for exam

--- src/java_project/ForTest.java
+++ src/java_project/ForTest.java
@@ -1,10 +1,41 @@
 package java_project;
 
+import java.util.*;
+
 public class ForTest {
 
-	public static void main(String[] args) {
-		for(int i = 0; i<3; i++) {
-			System.out.println("test!");
+	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		int input1 = sc.nextInt();
+		digitTest1(input1);
+		int input2 = sc.nextInt();
+		digitTest2(input2);
+	}
+	
+	public static void digitTest1(int num) {
+		int cnt = 1;
+		for(int i = 0; i<num; i++) {
+			for(int j = 0; j<i; j++) {
+				System.out.print("\t");
+			}
+			for(int k = 0; k<num-i; k++) {
+				System.out.print(cnt++ + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void digitTest2(int num) {
+		int cnt = 1;
+		
+		for(int i = num-1; Math.abs(i)<num; i-=2) {
+			for(int j = 0; j<(num-1-Math.abs(i))/2; j++) {
+				System.out.print("\t");
+			}
+			for(int j = 0; j<Math.abs(i)+1; j++) {
+				System.out.print(cnt++ + "\t");
+			}
+			System.out.println();
 		}
 	}
 
