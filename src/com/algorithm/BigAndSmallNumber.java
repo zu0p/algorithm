b89b6f2d01306741ace1619338d75dbcb9172a81
박주영 <zu0p@kakao.com>
2021-01-19 15:03:37 +0900

    create package algorithm

---
+++ src/com/algorithm/BigAndSmallNumber.java
@@ -0,0 +1,81 @@
+/*
+ * Writer: 주영,박
+ * 20210119
+ * 
+ * 백준_2992) 크면서 작은 수
+ * https://www.acmicpc.net/problem/2992
+ * 
+ * 순열
+ */
+package com.algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class BigAndSmallNumber {
+    static int min = Integer.MAX_VALUE;
+    static boolean isChanged = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // input
+        String input = br.readLine();
+        String[] str = input.split("");
+
+        int n = input.length();
+        int arr[] = new int[n];
+        int output[] = new int[n];
+        boolean visited[] = new boolean[n];
+        for(int i = 0; i<n; i++){
+            int tmp = Integer.valueOf(str[i]);
+            arr[i] = tmp;
+            visited[i] = false;
+        }
+        int t = arrToInt(arr);
+
+        perm(t, arr, output, visited, 0, n, n);
+        if(isChanged != true){
+            bw.write("0");
+        }
+        else{
+            bw.write(Integer.toString(min));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    static void perm(int origin, int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
+        if (depth == r) {
+                int tmp = arrToInt(output);
+                if(tmp>origin){
+                    if(tmp<min){
+                        min = tmp;
+                        isChanged = true;
+                    }
+                }
+                //print(output, r);
+                return;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (visited[i] != true) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                perm(origin, arr, output, visited, depth + 1, n, r);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static int arrToInt(int[] arr){
+        int res = 0;
+        int size = arr.length;
+        for(int i = 0; i<size; i++){
+            res += arr[i]*Math.pow(10,size-i-1);
+        }
+        return res;
+    }
+}
--- src/com/lecture/ForTest.java
+++ src/com/lecture/ForTest.java
@@ -1,4 +1,9 @@
-package java_project;
+/*
+ * Writer: 주영,박
+ * 20210118
+ * For문 연습 
+ */
+package com.lecture;
 
 import java.util.*;
 
@@ -28,8 +33,8 @@ public class ForTest {
 	public static void digitTest2(int num) {
 		int cnt = 1;
 		
-		for(int i = num-1; Math.abs(i)<num; i-=2) {
-			for(int j = 0; j<(num-1-Math.abs(i))/2; j++) {
+		for(int i = num-1; Math.abs(i)<num; i-=2) {			// 4 2 0 |-2| |-4|번씩
+			for(int j = 0; j<(num-1-Math.abs(i))/2; j++) {	// 0 1 2 1 0 번씩
 				System.out.print("\t");
 			}
 			for(int j = 0; j<Math.abs(i)+1; j++) {
