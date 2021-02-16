80333aaf74e7f4fe85ab4079f9195b7fc809bd2f
박주영 <zu0p@bagjuyeong-ui-MacBookAir.local>
2021-02-16 23:40:47 +0900

    백준 설탕배달 - greedy, (recursive)

--- src/com/algorithm/BOJ_2839.java
+++ src/com/algorithm/BOJ_2839.java
@@ -1,2 +1,25 @@
-package com.algorithm;public class BOJ_2839 {
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 4| n == 7){
+            System.out.println(-1);
+        }
+        else{
+            int cnt3 = 0;
+            while(n%5!=0 && n>0){
+                n -= 3;
+                cnt3++;
+            }
+            int cnt5 = n/5;
+            System.out.println(cnt5+cnt3);
+        }
+    }
 }
